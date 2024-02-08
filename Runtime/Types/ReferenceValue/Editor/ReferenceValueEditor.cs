12764dc7738946ca467f167b325145cec1db5bed
Shokhrukhkhon Rustamkhonov <rustamkhonoff@sabgames.org>
2024-02-08 18:24:45 +0500

    editor changed

--- Runtime/Types/ReferenceValue/Editor/ReferenceValueEditor.cs
+++ Runtime/Types/ReferenceValue/Editor/ReferenceValueEditor.cs
@@ -10,29 +10,55 @@ namespace Packages.PlanBArchitecture.Runtime.Types.ReferenceValue.Editor
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            Rect helpBoxRect = new(0, 0, position.width, 100);
+
             SerializedProperty useReference = property.FindPropertyRelative("_useReference");
             string propertyName = useReference.boolValue ? "_value1" : "_value2";
+            SerializedProperty propertyValue = property.FindPropertyRelative(propertyName);
 
-            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            Rect labelRect = position;
+            labelRect.width = position.width / 2f;
+            labelRect.height = EditorGUIUtility.singleLineHeight;
+
+            Rect useReferenceRect = position;
+            useReferenceRect.width = 0;
+            useReferenceRect.x = position.width;
+            useReferenceRect.height = EditorGUIUtility.singleLineHeight;
+
+            Rect valueRect = position;
+            valueRect.height = EditorGUI.GetPropertyHeight(propertyValue);
+            valueRect.y += EditorGUIUtility.singleLineHeight + 5;
+
+            GUIStyle style = new GUIStyle(EditorStyles.label)
             {
-                EditorGUILayout.BeginHorizontal();
-                {
-                    EditorGUILayout.LabelField(label);
-                    GUIStyle style = new(GUI.skin.label)
-                    {
-                        padding = new RectOffset(10, 10, 0, 0),
-                        alignment = TextAnchor.MiddleRight
-                    };
-                    EditorGUILayout.LabelField("Use Reference", style);
-                    EditorGUILayout.PropertyField(useReference, GUIContent.none);
-                }
-                EditorGUILayout.EndHorizontal();
-                EditorGUI.indentLevel++;
-                string valueLabel = useReference.boolValue ? "Reference Value" : "Override Value";
-                EditorGUILayout.PropertyField(property.FindPropertyRelative(propertyName), new GUIContent(valueLabel));
-                EditorGUI.indentLevel--;
-            }
-            EditorGUILayout.EndVertical();
+                alignment = TextAnchor.MiddleRight,
+                padding = new RectOffset(10, 10, 0, 0)
+            };
+            Rect indentedRect = EditorGUI.IndentedRect(position);
+
+            const int padding = 10;
+            indentedRect.x -= padding;
+            indentedRect.width += padding + 5;
+            indentedRect.height -= padding;
+
+            EditorGUI.HelpBox(indentedRect, "", MessageType.None);
+
+            EditorGUI.LabelField(labelRect, label);
+
+            Rect useReferenceLabelRect = new(useReferenceRect.x - 100, useReferenceRect.y, 100, useReferenceRect.height);
+            EditorGUI.LabelField(useReferenceLabelRect, "Use Reference", style);
+            EditorGUI.PropertyField(useReferenceRect, useReference, GUIContent.none);
+
+            EditorGUI.indentLevel++;
+            EditorGUI.PropertyField(valueRect, propertyValue, true);
+            EditorGUI.indentLevel--;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty useReference = property.FindPropertyRelative("_useReference");
+            string propertyName = useReference.boolValue ? "_value1" : "_value2";
+            return EditorGUI.GetPropertyHeight(property.FindPropertyRelative(propertyName)) + EditorGUIUtility.singleLineHeight * 2f + 5;
         }
     }
 }

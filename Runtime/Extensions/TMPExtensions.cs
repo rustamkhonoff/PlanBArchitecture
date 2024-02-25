3127fb291da386252ea37027ff3c7813f4b7e73d
rustamkhonoff <rustamkhonoff@gmail.com>
2024-02-25 23:28:14 +0500

    animated text reveal

--- Runtime/Extensions/TMPExtensions.cs
+++ Runtime/Extensions/TMPExtensions.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using TMPro;
 using UnityEngine;
 
@@ -12,7 +15,8 @@ namespace Extensions
         /// </summary>
         public static Vector3 GetSpritePosition(this TMP_Text tmpText, int spriteIndex)
         {
-            List<TMP_CharacterInfo> charsList = tmpText.textInfo.characterInfo.Where(a => a.elementType == TMP_TextElementType.Sprite).ToList();
+            List<TMP_CharacterInfo> charsList = tmpText.textInfo.characterInfo
+                .Where(a => a.elementType == TMP_TextElementType.Sprite).ToList();
             if (charsList.Count == 0)
             {
                 Debug.LogWarning("There is no any sprite in text", tmpText);
@@ -21,7 +25,8 @@ namespace Extensions
 
             foreach (TMP_CharacterInfo tmpCharacterInfo in charsList)
             {
-                if (tmpCharacterInfo.elementType == TMP_TextElementType.Sprite && tmpCharacterInfo.spriteIndex == spriteIndex)
+                if (tmpCharacterInfo.elementType == TMP_TextElementType.Sprite &&
+                    tmpCharacterInfo.spriteIndex == spriteIndex)
                 {
                     Vector3 spritePosition = Vector3.Lerp(tmpCharacterInfo.bottomLeft, tmpCharacterInfo.topRight, 0.5f);
                     return tmpText.transform.TransformPoint(spritePosition);
@@ -43,7 +48,8 @@ namespace Extensions
                 .Where(texture => texture.Value != null)
                 .ToDictionary(x => x.Key, x => x.Value);
 
-            List<TMP_CharacterInfo> charsList = tmpText.textInfo.characterInfo.Where(a => a.elementType == TMP_TextElementType.Sprite).ToList();
+            List<TMP_CharacterInfo> charsList = tmpText.textInfo.characterInfo
+                .Where(a => a.elementType == TMP_TextElementType.Sprite).ToList();
             if (charsList.Count == 0)
             {
                 Debug.LogWarning("There is no any sprite in text", tmpText);
@@ -52,14 +58,17 @@ namespace Extensions
 
             foreach (TMP_CharacterInfo tmpCharacterInfo in charsList)
             {
-                if (tmpCharacterInfo.elementType == TMP_TextElementType.Sprite && tmpCharacterInfo.spriteIndex == spriteIndex)
+                if (tmpCharacterInfo.elementType == TMP_TextElementType.Sprite &&
+                    tmpCharacterInfo.spriteIndex == spriteIndex)
                 {
                     TMP_SpriteAsset asset = tmpCharacterInfo.spriteAsset;
                     if (CachedTMPSpriteTextures.TryGetValue((asset, spriteIndex), out Texture2D cachedTexture))
                         return cachedTexture;
                     Texture2D texture = asset.spriteSheet as Texture2D;
-                    int width = (int)(Vector2.Distance(tmpCharacterInfo.bottomLeft, tmpCharacterInfo.bottomRight) / tmpCharacterInfo.scale);
-                    int height = (int)(Vector2.Distance(tmpCharacterInfo.bottomLeft, tmpCharacterInfo.topLeft) / tmpCharacterInfo.scale);
+                    int width = (int)(Vector2.Distance(tmpCharacterInfo.bottomLeft, tmpCharacterInfo.bottomRight) /
+                                      tmpCharacterInfo.scale);
+                    int height = (int)(Vector2.Distance(tmpCharacterInfo.bottomLeft, tmpCharacterInfo.topLeft) /
+                                       tmpCharacterInfo.scale);
                     int posX = (int)(tmpCharacterInfo.vertex_BL.uv.x * texture.height);
                     int posY = (int)(tmpCharacterInfo.vertex_BL.uv.y * texture.width);
                     Color[] colors = texture.GetPixels(posX, posY, width, height);
@@ -73,6 +82,39 @@ namespace Extensions
 
             Debug.LogWarning($"There is no character with given sprite index, index = {spriteIndex}", tmpText);
             return null;
+        }
+
+        public static void AnimateTextReveal(this TMP_Text tmp, string text, float perCharDelay = 0.01f,
+            Action endAction = null)
+        {
+            tmp.StartCoroutine(IEAnimatedTextReveal(tmp, text, perCharDelay, endAction));
+        }
+
+        public static IEnumerator IEAnimatedTextReveal(TMP_Text tmp, string text, float perCharDelay = 0.01f,
+            Action endAction = default)
+        {
+            bool richText = tmp.richText;
+            tmp.richText = true;
+            WaitForSeconds wfs = new(perCharDelay);
+            StringBuilder sb = new(text.Length);
+
+            for (int index = 0; index < text.Length; index++)
+            {
+                sb.Clear();
+                sb.Append(text[..(index + 1)]);
+                if (index + 1 < text.Length)
+                {
+                    sb.Append("<alpha=#00>");
+                    sb.Append(text[(index + 1)..]);
+                }
+
+                tmp.SetText(sb);
+                yield return wfs;
+            }
+
+            tmp.SetText(text);
+            tmp.richText = richText;
+            endAction?.Invoke();
         }
     }
 }

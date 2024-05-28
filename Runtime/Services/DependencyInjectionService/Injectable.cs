2ff4319d2f75913a0f18a3caad588bf5dd39838b
Shokhrukhkhon Rustamkhonov <rustamkhonoff@sabgames.org>
2024-05-28 15:46:05 +0500

    obsolete attribute

--- Runtime/Services/DependencyInjectionService/Injectable.cs
+++ Runtime/Services/DependencyInjectionService/Injectable.cs
@@ -3,23 +3,28 @@ using System;
 namespace DependencyInjectionService
 {
     //Only use with methods to keep the same style in using DI service
-    [AttributeUsage(AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Method)] [Obsolete("Use DI attribute instead")]
     public class Injectable :
 #if ZENJECT
         Zenject.InjectAttribute
 #else
     Attribute
 #endif
-    {
-    }
+    { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    // ReSharper disable once InconsistentNaming
+    public class DI : Injectable { }
 
     //For those who somehow want to keep injecting dependencies into anything
+    [Obsolete("Use DIAny attribute instead")]
     public class InjectableAny :
 #if ZENJECT
         Zenject.InjectAttribute
 #else
     Attribute
 #endif
-    {
-    }
+    { }
+
+    public class DIAny : InjectableAny { }
 }
